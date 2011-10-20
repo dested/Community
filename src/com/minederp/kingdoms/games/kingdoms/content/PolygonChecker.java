4278a8296f05281533b47eb80175d660a28c1e90
dested <dested@gmail.com>
2011-10-19 22:00:25 -0700

    fixed namespaces

--- src/com/minederp/kingdoms/games/kingdoms/content/PolygonChecker.java
+++
@@ -1,8 +0,0 @@
-package com.minederp.kingdoms.games.kingdoms.content;
-
-import com.minederp.kingdoms.util.Polygon;
-
-public interface PolygonChecker {
-	public void save(Polygon polygon);
-	public boolean collides(Polygon polygon);
-}
--- src/com/minederp/kingdoms/util/ChestWatcher.java
+++
@@ -1,5 +0,0 @@
-package com.minederp.kingdoms.util;
-
-public class ChestWatcher {
-	
-}
